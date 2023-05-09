379c02a60c82514d55899b595ff467b7f1893ef7
Raul Felix <raulfelixsousa@gmail.com>
2023-05-09 14:58:41 -0300

    Insert user with POST

--- src/main/java/com/raulfelix/workshopmongo/services/UserService.java
+++ src/main/java/com/raulfelix/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.raulfelix.workshopmongo.services;
 
 import com.raulfelix.workshopmongo.domain.User;
+import com.raulfelix.workshopmongo.dto.UserDTO;
 import com.raulfelix.workshopmongo.repositories.UserRepository;
 import com.raulfelix.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDto(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
